b0ca93a44f98a735eec2518f40d129563d53cd3e
Wesley Carvalho <84718917+wesleyeagles@users.noreply.github.com>
2024-11-03 14:41:46 -0300

    Add files via upload

---
+++ javascript/variables.js
@@ -0,0 +1,139 @@
+const specs = [
+    {
+        icon: `<svg width="30" height="24" viewBox="0 0 30 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M1 4.5874C1 3.78334 1.31941 3.0122 1.88798 2.44364C2.45654 1.87508 3.22767 1.55566 4.03174 1.55566H25.2539C26.058 1.55566 26.8291 1.87508 27.3977 2.44364C27.9662 3.0122 28.2856 3.78334 28.2856 4.5874V19.7461C28.2856 20.5502 27.9662 21.3213 27.3977 21.8899C26.8291 22.4584 26.058 22.7778 25.2539 22.7778H4.03174C3.22767 22.7778 2.45654 22.4584 1.88798 21.8899C1.31941 21.3213 1 20.5502 1 19.7461V4.5874Z" stroke="white" stroke-width="1.6" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M5.54688 6.10315H5.56299" stroke="white" stroke-width="1.6" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M10.0957 6.10315H10.1118" stroke="white" stroke-width="1.6" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+`,
+        name: "Tela",
+        description: `5"IPS tela de toque capacitiva`
+    },
+
+    {
+        icon: `<svg width="23" height="23" viewBox="0 0 23 23" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M2.3125 1H20.6875C21.0356 1 21.3694 1.13828 21.6156 1.38442C21.8617 1.63056 22 1.9644 22 2.3125V8.875C22 9.2231 21.8617 9.55694 21.6156 9.80308C21.3694 10.0492 21.0356 10.1875 20.6875 10.1875H11.5C11.1519 10.1875 10.8181 10.3258 10.5719 10.5719C10.3258 10.8181 10.1875 11.1519 10.1875 11.5V20.6875C10.1875 21.0356 10.0492 21.3694 9.80308 21.6156C9.55694 21.8617 9.2231 22 8.875 22H2.3125C1.9644 22 1.63056 21.8617 1.38442 21.6156C1.13828 21.3694 1 21.0356 1 20.6875V2.3125C1 1.9644 1.13828 1.63056 1.38442 1.38442C1.63056 1.13828 1.9644 1 2.3125 1Z" stroke="white" stroke-width="1.6" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M1 6.25H3.625" stroke="white" stroke-width="1.6" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M1 11.5H4.9375" stroke="white" stroke-width="1.6" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M1 16.75H3.625" stroke="white" stroke-width="1.6" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M6.25 1V3.625" stroke="white" stroke-width="1.6" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M11.5 1V4.9375" stroke="white" stroke-width="1.6" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M16.75 1V3.625" stroke="white" stroke-width="1.6" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+`,
+        name: "Dimensões do produto",
+        description: `254*215*71mm`
+    },
+
+    {
+        icon: `<svg width="27" height="29" viewBox="0 0 27 29" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M8.62708 5.76471C8.62708 7.02838 9.12907 8.24031 10.0226 9.13386C10.9162 10.0274 12.1281 10.5294 13.3918 10.5294C14.6555 10.5294 15.8674 10.0274 16.7609 9.13386C17.6545 8.24031 18.1565 7.02838 18.1565 5.76471C18.1565 4.50103 17.6545 3.28911 16.7609 2.39555C15.8674 1.50199 14.6555 1 13.3918 1C12.1281 1 10.9162 1.50199 10.0226 2.39555C9.12907 3.28911 8.62708 4.50103 8.62708 5.76471Z" stroke="white" stroke-width="1.6" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M5.18842 10.5294H21.5949C21.9668 10.5294 22.327 10.6599 22.6126 10.8981C22.8982 11.1364 23.0911 11.4674 23.1577 11.8334L25.7576 26.1275C25.7993 26.3564 25.7901 26.5917 25.7308 26.8166C25.6715 27.0416 25.5635 27.2508 25.4144 27.4295C25.2653 27.6081 25.0788 27.7518 24.8681 27.8504C24.6573 27.949 24.4275 28 24.1948 28H2.58847C2.35581 28 2.12597 27.949 1.91522 27.8504C1.70448 27.7518 1.51797 27.6081 1.36888 27.4295C1.2198 27.2508 1.11178 27.0416 1.05247 26.8166C0.993159 26.5917 0.984004 26.3564 1.02565 26.1275L3.62559 11.8334C3.69217 11.4674 3.88508 11.1364 4.17068 10.8981C4.45628 10.6599 4.81646 10.5294 5.18842 10.5294Z" stroke="white" stroke-width="1.6" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+
+`,
+        name: "Peso",
+        description: `3.8lb /1.73kg`
+    },
+
+    {
+        icon: `<svg width="28" height="26" viewBox="0 0 28 26" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M3.62068 14.7586C2.62146 15.3355 1.84052 16.226 1.39898 17.292C0.957435 18.3579 0.879971 19.5398 1.1786 20.6543C1.47722 21.7688 2.13525 22.7536 3.05063 23.456C3.966 24.1584 5.08756 24.5391 6.24137 24.5391C7.39517 24.5391 8.51673 24.1584 9.43211 23.456C10.3475 22.7536 11.0055 21.7688 11.3041 20.6543C11.6028 19.5398 11.5253 18.3579 11.0838 17.292C10.6422 16.226 9.86127 15.3355 8.86205 14.7586V3.62068C8.86205 2.92563 8.58594 2.25905 8.09447 1.76758C7.603 1.27611 6.93642 1 6.24137 1C5.54632 1 4.87974 1.27611 4.38826 1.76758C3.89679 2.25905 3.62068 2.92563 3.62068 3.62068V14.7586Z" stroke="white" stroke-width="1.6" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M3.62109 8.86206H8.86246" stroke="white" stroke-width="1.6" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M17.7071 2.3103L19.0174 4.93099H21.9657M20.6552 2.3103L16.7242 8.86201L19.3448 12.793M19.3448 12.793L27.2069 12.793M19.3448 12.793L16.7242 16.7241M24.9139 10.1724L23.2759 12.7931L24.9139 15.4138M16.7242 16.7241L20.6552 23.2757M16.7242 16.7241L15.4138 16.724M21.9657 20.6551H19.0174L17.7071 23.2758M14.1035 8.86208L16.8395 8.87257" stroke="white" stroke-opacity="0.64" stroke-width="1.6" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+`,
+        name: "Temperatura de trabalho",
+        description: `-14~122°F/-10~50°C`
+    },
+
+    {
+        icon: `<svg width="24" height="31" viewBox="0 0 24 31" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M18.2455 20.3108H21.2447M18.2455 23.6849C17.6737 24.015 17.2269 24.5246 16.9742 25.1345C16.7216 25.7445 16.6772 26.4208 16.8481 27.0585C17.019 27.6963 17.3955 28.2598 17.9193 28.6617C18.4431 29.0636 19.0849 29.2815 19.7451 29.2815C20.4054 29.2815 21.0471 29.0636 21.5709 28.6617C22.0947 28.2598 22.4713 27.6963 22.6421 27.0585C22.813 26.4208 22.7687 25.7445 22.516 25.1345C22.2634 24.5246 21.8165 24.015 21.2447 23.6849V17.3116C21.2447 16.9138 21.0867 16.5324 20.8055 16.2512C20.5243 15.9699 20.1428 15.812 19.7451 15.812C19.3474 15.812 18.966 15.9699 18.6847 16.2512C18.4035 16.5324 18.2455 16.9138 18.2455 17.3116V23.6849Z" stroke="white" stroke-width="1.6" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M11.9971 25.7435L1 19.5576V7.18588L11.9971 1L22.9942 7.18588V13.3718" stroke="white" stroke-width="1.6" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M11.998 13.3717L22.9952 7.18579" stroke="white" stroke-width="1.6" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M11.998 13.3716V25.7433" stroke="white" stroke-width="1.6" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M11.9973 13.3717L1.00014 7.18585" stroke="white" stroke-width="1.6" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+
+`,
+        name: "Temperatura de armazenamento",
+        description: `-4~140°F/-20~60°C`
+    },
+
+    {
+        icon: `<svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M1 7.42859H19" stroke="white" stroke-width="1.6" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M1 12.5714H19" stroke="white" stroke-width="1.6" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M1 19L19 1" stroke="white" stroke-width="1.6" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+`,
+        name: "Resolução",
+        description: `0-400 1 microns; 400-3000 10 microns; 3000-10000 100 microns; 10000-19000 250 microns`
+    },
+
+    {
+        icon: `<svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M1 6V3.5C1 2.83696 1.26339 2.20107 1.73223 1.73223C2.20107 1.26339 2.83696 1 3.5 1H6" stroke="white" stroke-width="1.6" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M1 16V18.5C1 19.163 1.26339 19.7989 1.73223 20.2678C2.20107 20.7366 2.83696 21 3.5 21H6" stroke="white" stroke-width="1.6" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M16 1H18.5C19.163 1 19.7989 1.26339 20.2678 1.73223C20.7366 2.20107 21 2.83696 21 3.5V6" stroke="white" stroke-width="1.6" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M16 21H18.5C19.163 21 19.7989 20.7366 20.2678 20.2678C20.7366 19.7989 21 19.163 21 18.5V16" stroke="white" stroke-width="1.6" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M7.25 11H14.75" stroke="white" stroke-width="1.6" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M11 7.25V14.75" stroke="white" stroke-width="1.6" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+`,
+        name: "Precisão",
+        description: `1-10000 microns: ±10%; 10000-19000 microns: ±20%`
+    },
+
+    {
+        icon: `<svg width="24" height="13" viewBox="0 0 24 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M6.11111 1.77783L1 6.88894L6.11111 12.0001" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M17.8889 1.77783L23 6.88894L17.8889 12.0001" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M1 7L22 7.00529" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+
+`,
+        name: "Faixa de medição de vácuo",
+        description: `1-19000 microns`
+    },
+
+    {
+        icon: `<svg width="26" height="28" viewBox="0 0 26 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M1 14.9999C1 16.5492 1.30517 18.0834 1.89808 19.5148C2.49099 20.9462 3.36003 22.2469 4.45559 23.3424C5.55114 24.438 6.85176 25.307 8.28317 25.8999C9.71458 26.4928 11.2488 26.798 12.7981 26.798C14.3475 26.798 15.8816 26.4928 17.3131 25.8999C18.7445 25.307 20.0451 24.438 21.1406 23.3424C22.2362 22.2469 23.1052 20.9462 23.6981 19.5148C24.2911 18.0834 24.5962 16.5492 24.5962 14.9999C24.5962 11.8708 23.3532 8.86994 21.1406 6.65737C18.9281 4.44479 15.9272 3.20178 12.7981 3.20178C9.66906 3.20178 6.66816 4.44479 4.45559 6.65737C2.24301 8.86994 1 11.8708 1 14.9999Z" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M17.0105 10.7861L12.7969 14.9997" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M12.2768 2.21392L12.3043 1.00009" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+
+`,
+        name: "Unidade de vácuo",
+        description: `micron、inHg、Torr、psia、mbar、mTorr、Pa、kPa`
+    },
+
+    {
+        icon: `<svg width="25" height="28" viewBox="0 0 25 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M22.5622 6.64904C23.0032 6.90482 23.3686 7.27289 23.6211 7.71577C23.8736 8.15866 24.0042 8.66055 23.9997 9.17034V18.5929C23.9997 19.6393 23.4333 20.6053 22.5191 21.1127L13.8943 26.6383C13.4679 26.8756 12.988 27.0001 12.5 27.0001C12.012 27.0001 11.5321 26.8756 11.1057 26.6383L2.48095 21.1127C2.03224 20.8628 1.65841 20.4976 1.39812 20.0549C1.13783 19.6121 1.00051 19.1079 1.00037 18.5943V9.1689C1.00037 8.12244 1.56672 7.1579 2.48095 6.64904L11.1057 1.50008C11.545 1.25445 12.0398 1.12549 12.5431 1.12549C13.0464 1.12549 13.5413 1.25445 13.9806 1.50008L22.6053 6.64904H22.5622Z" stroke="white" stroke-width="1.6" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M8.18765 14.0664C8.18765 15.2101 8.64199 16.307 9.45071 17.1157C10.2594 17.9244 11.3563 18.3788 12.5 18.3788C13.6437 18.3788 14.7406 17.9244 15.5493 17.1157C16.358 16.307 16.8124 15.2101 16.8124 14.0664C16.8124 12.9227 16.358 11.8258 15.5493 11.0171C14.7406 10.2084 13.6437 9.75403 12.5 9.75403C11.3563 9.75403 10.2594 10.2084 9.45071 11.0171C8.64199 11.8258 8.18765 12.9227 8.18765 14.0664Z" stroke="white" stroke-width="1.6" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+
+`,
+        name: "Conectores vacuômetro",
+        description: `1/4SAE e 1/4 SAE T`
+    },
+]
+
+const listSpec = document.querySelector(".list-spec")
+
+specs.forEach((spec) => {
+    listSpec.innerHTML += `
+    <div class="flex flex-col gap-4 items-center lg:items-start">
+        <span class="material-symbols-outlined text-3xl border border-[#FFFFFF32] border-2 flex items-center justify-center w-[55px] h-[55px] rounded-full">
+            ${spec.icon}
+        </span>
+        <div class="flex flex-col">
+            <span class="font-medium text-lg text-white block max-w-[150px] text-center lg:text-start">${spec.name}</span>
+            <span class="text-[#FFFFFF64] block mt-4 max-w-[170px] text-center lg:text-start">${spec.description}</span>
+        </div>
+    </div>
+    `
+})
